9f8c62a7524a7949a13e2da1bde99631ba896ac2
boris_vujica <borisvujica7@gmail.com>
2026-05-24 19:55:51 +0200

    Fix production deployment

--- server.js
+++ server.js
@@ -5,18 +5,23 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/authRoutes.js";
 import workoutRoutes from "./routes/workoutRoutes.js";
-
 import exerciseRoutes from "./routes/exerciseRoutes.js";
+
 import seedExercises from "./seedExercises.js";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.NODE_ENV === "production" ? "https://active-zone-frontend.vercel.app/" : "http://localhost:3000",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://active-zone-frontend.vercel.app"
+        : "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
@@ -24,19 +29,19 @@ app.use("/api/auth", authRoutes);
 app.use("/api/workouts", workoutRoutes);
 app.use("/api/exercises", exerciseRoutes);
 
+const PORT = process.env.PORT || 5000;
+
 const startServer = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB connected");
 
-    
     await seedExercises();
 
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
-
   } catch (err) {
     console.error(err);
   }
