c703613e381026d6a6c54c2f1139c3c2221e1486
boris_vujica <borisvujica7@gmail.com>
2026-06-13 20:22:52 +0200

    zadnje promjene vjerovatno

--- models/Exercise.js
+++ models/Exercise.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
+
 const exerciseSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  category: {
-    type: String,
-    required: true
-  },
-  muscle: {
-    type: String,
-    required: true
-    },
+  name: { type: String, required: true },
+  category: { type: String, required: true },
+  muscle: { type: String, required: true },
+
+  isCustom: { type: Boolean, default: false },
+
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    default: null
+  }
 });
 
 export default mongoose.model("Exercise", exerciseSchema);
--- routes/exerciseRoutes.js
+++ routes/exerciseRoutes.js
@@ -1,14 +1,59 @@
 import express from "express";
 import Exercise from "../models/Exercise.js";
+import auth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+
+router.get("/", auth, async (req, res) => {
   try {
-    const exercises = await Exercise.find();
+    const exercises = await Exercise.find({
+      $or: [
+        { isCustom: false },
+        { user: req.user.id }
+      ]
+    });
 
     res.json(exercises);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+
+router.post("/", auth, async (req, res) => {
+  try {
+    const { name, category } = req.body;
+
+    const exercise = new Exercise({
+      name,
+      category,
+      muscle: category,
+      isCustom: true,
+      user: req.user.id
+    });
+
+    await exercise.save();
+    res.json(exercise);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const ex = await Exercise.findOne({
+      _id: req.params.id,
+      user: req.user.id
+    });
+
+    if (!ex) {
+      return res.status(404).json({ message: "Not found" });
+    }
+
+    await ex.deleteOne();
+    res.json({ message: "Deleted" });
   } catch (err) {
     res.status(500).json(err);
   }
